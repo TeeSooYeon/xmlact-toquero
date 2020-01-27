b545690291653436e79e52b0fb53b9326b3f73cd
TeeSooYeon <60122556+TeeSooYeon@users.noreply.github.com>
2020-01-27 23:13:35 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+
+	$rss .='<?xml version="1.0" encoding="UTF-8"?>';
+	$rss .='<rss version ="2.0">';
+	$rss .='<channel>';
+
+	$conn = mysqli_connect("dbrojasdev.cjw42bnplsor.us-east-1.rds.amazonaws.com","admin","root1234","db_1820921") or die(mysqli_error($conn));
+
+	$sql = "SELECT * FROM tbl_viruses";
+	$query = mysqli_query($conn,$sql) or die (mysqli_error($conn));
+	
+	while($record = mysqli_fetch_assoc($query)){
+		extract($record);
+		
+		$rss.='<viruses>';
+		$rss.='<name>'. $v_name .'</name>';
+		$rss.='<desc>'. $v_desc.'</desc>';
+		$rss.='<cure>'. $v_cure .'</cure>';
+		$rss.='</viruses>';
+	}
+	$rss.='</channel>';
+	$rss.='</rss>';
+	
+	echo $rss;
+?>
