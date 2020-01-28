2ae95b319ed6d246cf751b5e9f64edba3f8fd50e
TeeSooYeon <60122556+TeeSooYeon@users.noreply.github.com>
2020-01-28 11:19:01 +0800

    Add files via upload

---
+++ index_2.php
@@ -0,0 +1,23 @@
+<?php
+	$domOBJ = new DOMDocument();
+	$domOBJ->load("http://demo-app-tsy.herokuapp.com/rss.php");
+	
+	$content = $domOBJ->getElementsbyTagName("viruses");
+	
+?>
+<ul>
+	<?php
+		foreach($content as $data){
+			$vname = $data->getElementsByTagName("name")->item(0)->nodeValue;
+			$vdesc = $data->getElementsByTagName("desc")->item(0)->nodeValue;
+			$vcure = $data->getElementsByTagName("cure")->item(0)->nodeValue;
+			
+			echo"<li>Virus:$vname
+					<ul>
+						<li>Virus Description: $vdesc</li>
+						<li>Cure: $vcure</li>
+					</ul>
+				</li>";
+		}
+	?>
+</ul>
